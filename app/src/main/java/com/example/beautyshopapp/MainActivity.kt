836eb1f0c4df5d7d8346b2ce70eab77e32a7c4ba
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-09 18:32:09 +0600

    main activity.kt bug fix

--- app/src/main/java/com/example/beautyshopapp/MainActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.beautyshopapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null) {
+
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+
+        } else {
+
+            setContentView(R.layout.activity_main)
+
+        }
     }
 }
