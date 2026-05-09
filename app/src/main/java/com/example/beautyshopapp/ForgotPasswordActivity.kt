8188da5957fc192da5015713ea59015425954ae3
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-09 17:54:22 +0600

    forgot password.xml add

--- app/src/main/java/com/example/beautyshopapp/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/ForgotPasswordActivity.kt
@@ -1,20 +1,62 @@
 package com.example.beautyshopapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = FirebaseAuth.getInstance()
+
+        val emailEt = findViewById<EditText>(R.id.emailEt)
+        val resetBtn = findViewById<Button>(R.id.resetBtn)
+
+        resetBtn.setOnClickListener {
+
+            val email = emailEt.text.toString().trim()
+
+            if (email.isEmpty()) {
+
+                Toast.makeText(
+                    this,
+                    "Enter your email",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+
+                        if (task.isSuccessful) {
+
+                            Toast.makeText(
+                                this,
+                                "Reset Email Sent",
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+
+                        } else {
+
+                            Toast.makeText(
+                                this,
+                                task.exception?.message,
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+            }
         }
     }
 }
