7734d926469392cba35846738a27ff95f85fe84b
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-14 12:06:56 +0600

    splashactivity.kt & splashactivity.xml add

--- app/src/main/java/com/example/beautyshopapp/SplashActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/SplashActivity.kt
@@ -1,20 +1,33 @@
 package com.example.beautyshopapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.beautyshopapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.apply {
+
+            startBtn.setOnClickListener {
+
+                startActivity(
+                    Intent(this@SplashActivity, LoginActivity::class.java)
+                )
+
+            }
         }
     }
 }
