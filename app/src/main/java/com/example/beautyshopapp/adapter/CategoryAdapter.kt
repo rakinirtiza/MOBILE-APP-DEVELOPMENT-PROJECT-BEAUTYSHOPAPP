9c586e95475479094a221238d628e36a7d884764
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-15 09:10:56 +0600

    bug fix categoryadapter.kt

--- app/src/main/java/com/example/beautyshopapp/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/beautyshopapp/adapter/CategoryAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.beautyshopapp.adapter
 
-class CategoryAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.beautyshopapp.domain.CategoryModel
+
+class CategoryAdapter(val items: List<CategoryModel>) :
+    RecyclerView.Adapter<CategoryAdapter.Viewholder>() {
+
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
+
+    class Viewholder(val binding: ) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoryAdapter.Viewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CategoryAdapter.Viewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
