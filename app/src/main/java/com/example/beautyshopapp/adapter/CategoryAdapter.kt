aeb7d6713d8be8720094f92811ca04e561db65c1
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-15 11:08:59 +0600

    categoryadapter.kt update for bug fixing

--- app/src/main/java/com/example/beautyshopapp/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/beautyshopapp/adapter/CategoryAdapter.kt
@@ -1,31 +1,73 @@
 package com.example.beautyshopapp.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.beautyshopapp.R
+import com.example.beautyshopapp.databinding.ViewholderCategoryBinding
 import com.example.beautyshopapp.domain.CategoryModel
 
 class CategoryAdapter(val items: List<CategoryModel>) :
-    RecyclerView.Adapter<CategoryAdapter.Viewholder>() {
+    RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     private var selectedPosition = -1
     private var lastSelectedPosition = -1
 
-    class Viewholder(val binding: ) {
-
-    }
+    class ViewHolder(val binding: ViewholderCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): CategoryAdapter.Viewholder {
-        TODO("Not yet implemented")
+    ): ViewHolder {
+
+        val binding = ViewholderCategoryBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CategoryAdapter.Viewholder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val item = items[position]
+        holder.binding.titleTxt.text = item.title
+
+        if (selectedPosition == position) {
+
+            holder.binding.titleTxt.setBackgroundResource(R.drawable.purple_bg)
+
+            holder.binding.titleTxt.setTextColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.darkGrey
+                )
+            )
+
+        } else {
+
+            holder.binding.titleTxt.setBackgroundResource(R.drawable.white_bg)
+
+            holder.binding.titleTxt.setTextColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.black
+                )
+            )
+        }
+
+        holder.binding.root.setOnClickListener {
+
+            lastSelectedPosition = selectedPosition
+            selectedPosition = holder.adapterPosition
+
+            notifyItemChanged(lastSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 }
