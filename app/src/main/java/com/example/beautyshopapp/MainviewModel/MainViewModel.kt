9e2e5a5be8d9800c696838da989d807bfde1ab08
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-16 08:58:58 +0600

    add popular list

--- app/src/main/java/com/example/beautyshopapp/MainviewModel/MainViewModel.kt
+++ app/src/main/java/com/example/beautyshopapp/MainviewModel/MainViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.beautyshopapp.domain.CategoryModel
+import com.example.beautyshopapp.domain.ItemsModel
 import com.example.beautyshopapp.domain.SliderModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
 
 class MainViewModel : ViewModel() {
@@ -16,9 +18,12 @@ class MainViewModel : ViewModel() {
 
     private val _category = MutableLiveData<List<CategoryModel>>()
     private val _banner = MutableLiveData<List<SliderModel>>()
+    private val _popular = MutableLiveData<List<ItemsModel>>()
+
 
     val category: LiveData<List<CategoryModel>> = _category
     val banner: LiveData<List<SliderModel>> = _banner
+    val popular: LiveData<List<ItemsModel>> = _popular
 
     fun loadCategory() {
 
@@ -72,6 +77,39 @@ class MainViewModel : ViewModel() {
 
             override fun onCancelled(error: DatabaseError) {
 
+            }
+        })
+    }
+
+    fun loadPopular() {
+
+        val ref = firebaseDatabase.getReference("Items")
+
+        val query: Query = ref.orderByChild("showRecommended").equalTo(true)
+
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val lists = mutableListOf<ItemsModel>()
+
+                for (childSnapshot in snapshot.children) {
+
+                    val list =
+                        childSnapshot.getValue(ItemsModel::class.java)
+
+                    if (list != null) {
+
+                        lists.add(list)
+                    }
+                }
+
+                _popular.value = lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                TODO("Not yet implemented")
             }
         })
     }
