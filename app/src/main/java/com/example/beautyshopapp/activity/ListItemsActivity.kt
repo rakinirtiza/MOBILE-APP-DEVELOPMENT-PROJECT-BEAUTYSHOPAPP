049b0dc4208a264b849a5b6c290d718d36b059c0
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-17 08:54:55 +0600

    bug fix listitemactivity.kt

--- app/src/main/java/com/example/beautyshopapp/activity/ListItemsActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/activity/ListItemsActivity.kt
@@ -6,16 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.beautyshopapp.R
+import com.example.beautyshopapp.databinding.ActivityListItemsBinding
+import com.example.beautyshopapp.viewModel.MainViewModel
 
 class ListItemsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityListItemsBinding
+    private val viewModel= MainViewModel()
+    private var id: String=""
+    private var title: String=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_list_items)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       binding= ActivityListItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
