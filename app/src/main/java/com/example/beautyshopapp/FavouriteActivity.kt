60f34b060bae2e49ac0328ec59f1cb224bb4f39c
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-18 15:22:08 +0600

    favourite product adding complete

--- app/src/main/java/com/example/beautyshopapp/FavouriteActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/FavouriteActivity.kt
@@ -1,12 +1,39 @@
 package com.example.beautyshopapp
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FavouriteActivity : AppCompatActivity() {
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite)
+
+        val favouriteTxt = findViewById<TextView>(R.id.favouriteTxt)
+
+        sharedPreferences =
+            getSharedPreferences("FAVOURITE", MODE_PRIVATE)
+
+        val allItems = sharedPreferences.all
+
+        var result = ""
+
+        for ((key, value) in allItems) {
+
+            result += "$value\n\n"
+        }
+
+        if (result.isEmpty()) {
+
+            favouriteTxt.text = "No favourite items yet"
+
+        } else {
+
+            favouriteTxt.text = result
+        }
     }
 }
