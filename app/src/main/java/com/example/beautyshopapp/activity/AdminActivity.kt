1f9d7dccc5245e9097313e50443ddf968727917b
rakinirtiza <rakinisratirtiza@gmail.com>
2026-05-18 23:52:57 +0600

    admin update product item price everything

--- app/src/main/java/com/example/beautyshopapp/activity/AdminActivity.kt
+++ app/src/main/java/com/example/beautyshopapp/activity/AdminActivity.kt
@@ -1,13 +1,71 @@
 package com.example.beautyshopapp.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.beautyshopapp.R
+import com.google.firebase.database.FirebaseDatabase
 
 class AdminActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_admin)
+
+        val productIdEdt =
+            findViewById<EditText>(R.id.productIdEdt)
+
+        val newPriceEdt =
+            findViewById<EditText>(R.id.newPriceEdt)
+
+        val updatePriceBtn =
+            findViewById<Button>(R.id.updatePriceBtn)
+
+        updatePriceBtn.setOnClickListener {
+
+            val productId =
+                productIdEdt.text.toString().trim()
+
+            val newPrice =
+                newPriceEdt.text.toString().trim()
+
+            if (productId.isEmpty() || newPrice.isEmpty()) {
+
+                Toast.makeText(
+                    this,
+                    "Please fill all fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                return@setOnClickListener
+            }
+
+            FirebaseDatabase.getInstance()
+                .getReference("Items")
+                .child(productId)
+                .child("price")
+                .setValue(newPrice.toInt())
+
+                .addOnSuccessListener {
+
+                    Toast.makeText(
+                        this,
+                        "Price Updated Successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                .addOnFailureListener {
+
+                    Toast.makeText(
+                        this,
+                        "Update Failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
     }
 }
